e914be77081566823935549f846cbb3c1e76e630
Shreepad Murthy <68787197+SSMurthy619@users.noreply.github.com>
2021-08-25 14:34:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import jsonify
 import requests
-import pickle
+import pickle 
 import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
